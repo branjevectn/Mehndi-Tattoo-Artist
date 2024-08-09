8d8f2f52c8397ea7e7bdf7dcd89019781c409442
branjevectn <3105729+branjevectn@users.noreply.github.com>
2024-08-09 20:50:40 +0500

    Add files via upload

---
+++ site-info.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package Mehndi Tattoo Artist
+ * @subpackage mehndi_tattoo_artist
+ */
+
+?>
+
+<div class="site-info">
+    <div class="container">
+      <p><?php echo esc_html(get_theme_mod('mehndi_tattoo_artist_footer_text', __('Mehndi Tattoo Artist WordPress Theme By Themespride', 'mehndi-tattoo-artist'))); ?></p>
+    </div>
+</div>

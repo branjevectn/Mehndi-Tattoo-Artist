cf40a2ebab29470944bd959692bc6c96049f3a08
branjevectn <3105729+branjevectn@users.noreply.github.com>
2024-08-10 19:26:54 +0500

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,6 +1,6 @@
 <?php
 /**
-gfrhugdgniirfghddghrvjtndzftfsghhbbffgbvhjtdxdsyjjfhjddfhjddegjfghjfdd
+gdhfrhugdgniirfghddghrvjtndzftfsghhbbffgbvhjtdxdsyjjfhjddfhjddegjfghjfdd
  * The template for displaying comments
  *
  * @package Mehndi Tattoo Artist
